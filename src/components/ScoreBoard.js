f752bc1677c45e6e2d25fffd7737b228793e1f9d
lisahuele <lisahuele@gmail.com>
2021-09-16 18:14:36 -0400

    connection for scoreboard

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { pause, resume, restart } from '../actions';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 
 export default function ScoreBoard(props) {
+    const dispatch = useDispatch();
+    const game = useSelector((state => state.game));
+    const { score, isRunning, gameOver } = game;
+
     return (
         <div className="score-board">
-            <div>Score:{ props.score }</div>
+            <div>Score:{ score }</div>
             <div>Level: 1</div>
             <button className="score-board-button" onClick={(e) => {
-            }}> 
-                <FontAwesomeIcon icon={faPlay}/>
-                <FontAwesomeIcon icon={faPause}/> 
+                if(gameOver) {return}
+                if(isRunning) {
+                    dispatch(pause());
+                } else {
+                    dispatch(resume());
+                }
+            }}> {isRunning ? <FontAwesomeIcon icon={faPause}/> : <FontAwesomeIcon icon={faPlay}/> }
             </button>
+
             <button className="score-board-button" onClick={(e) => {
+                dispatch(restart())
             }}> Restart </button>
         </div>
     )
