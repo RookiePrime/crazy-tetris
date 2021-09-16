de208dd9658859e210db2585d1fd9e22af263688
lisahuele <lisahuele@gmail.com>
2021-09-16 16:49:08 -0400

    state utils

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+
 import './App.css';
 
-import Controls from './components/Controls';
-import GridBoard from './components/GridBoard';
-import NextBlock from './components/NextBlock';
-import PausePopup from './components/PausePopup';
-import ScoreBoard from './components/ScoreBoard';
+import GridBoard from './components/GridBoard'
+import NextBlock from './components/NextBlock'
+import ScoreBoard from './components/ScoreBoard'
+import Controls from './components/Controls'
+import PausePopup from './components/PausePopup'
+
+const store = createStore(reducers)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Tetris Redux</h1>
-      </header>
-      <GridBoard />
-      <NextBlock />
-      <ScoreBoard />
-      <Controls />
-      <PausePopup />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tetris Redux</h1>
+        </header>
+        <GridBoard />
+        <NextBlock />
+        <ScoreBoard />
+        <Controls />
+        <PausePopup />
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/utils/index.js
@@ -0,0 +1,153 @@
+export const random = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+// Returns the default grid
+export const gridDefault = () => {
+    const rows = 18
+    const cols = 10
+    const array = []
+  
+    for (let row = 0; row < rows; row++) {
+        array.push([])
+        for (let col = 0; col < cols; col++) {
+          array[row].push(0)
+        }
+    }
+  
+    return array
+}
+
+// Define block shapes and their rotations as arrays.
+export const shapes = [
+    // none
+    [[[0,0,0,0],
+      [0,0,0,0],
+      [0,0,0,0],
+      [0,0,0,0]]],
+  
+    // I
+    [[[0,0,0,0],
+      [1,1,1,1],
+      [0,0,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [0,1,0,0],
+      [0,1,0,0],
+      [0,1,0,0]]],
+  
+    // T
+    [[[0,0,0,0],
+      [1,1,1,0],
+      [0,1,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [1,1,0,0],
+      [0,1,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [1,1,1,0],
+      [0,0,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [0,1,1,0],
+      [0,1,0,0],
+      [0,0,0,0]]],
+  
+    // L
+    [[[0,0,0,0],
+      [1,1,1,0],
+      [1,0,0,0],
+      [0,0,0,0]],
+  
+     [[1,1,0,0],
+      [0,1,0,0],
+      [0,1,0,0],
+      [0,0,0,0]],
+  
+     [[0,0,1,0],
+      [1,1,1,0],
+      [0,0,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [0,1,0,0],
+      [0,1,1,0],
+      [0,0,0,0]]],
+  
+    // J
+    [[[1,0,0,0],
+      [1,1,1,0],
+      [0,0,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,1,0],
+      [0,1,0,0],
+      [0,1,0,0],
+      [0,0,0,0]],
+  
+     [[0,0,0,0],
+      [1,1,1,0],
+      [0,0,1,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [0,1,0,0],
+      [1,1,0,0],
+      [0,0,0,0]]],
+  
+    // Z
+    [[[0,0,0,0],
+      [1,1,0,0],
+      [0,1,1,0],
+      [0,0,0,0]],
+  
+     [[0,0,1,0],
+      [0,1,1,0],
+      [0,1,0,0],
+      [0,0,0,0]]],
+  
+    // S
+    [[[0,0,0,0],
+      [0,1,1,0],
+      [1,1,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [0,1,1,0],
+      [0,0,1,0],
+      [0,0,0,0]]],
+  
+    // O
+    [[[0,1,1,0],
+      [0,1,1,0],
+      [0,0,0,0],
+      [0,0,0,0]]]
+]
+
+// Random Shape
+export const randomShape = () => {
+    return random(1, shapes.length - 1)
+}
+
+// function to return the default object with properties to reflect default game state
+export const defaultState = () => {
+    return {
+        grid: gridDefault(),
+        shape: randomShape(),
+        rotation: 0,
+        // put the shape in the center of the grid, above the top
+        x: 5,
+        y: -4,
+        nextShape: randomShape(),
+        score: 0,
+        speed: 1000,
+        // Tell the game that it's currently running
+        isRunning: true,
+        gameOver: false
+    }
+}
