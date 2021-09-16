6a085edf1e3c5c4c025d400a108cd043ebcc354c
lisahuele <lisahuele@gmail.com>
2021-09-16 16:08:08 -0400

    initial controls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Controls from './components/Controls';
 
 import GridBoard from './components/GridBoard';
 import NextBlock from './components/NextBlock';
@@ -14,6 +15,7 @@ function App() {
       <GridBoard />
       <NextBlock />
       <ScoreBoard />
+      <Controls />
     </div>
   );
 }
---
+++ src/components/Controls.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Controls(props) {
+    return (
+        <div className="controls">
+            {/* Left */}
+            <button className="control-button" onClick={(e) => {
+
+            }}> Left </button>
+
+            {/* Right */}
+            <button className="control-button" onClick={(e) => {
+
+            }}> Right </button>
+
+            {/* Rotate */}
+            <button className="control-button" onClick={(e) => {
+
+            }}> Rotate </button>
+
+            {/* Down */}
+            <button className="control-button" onClick={(e) => {
+
+            }}> Down </button>
+
+        </div>
+    )
+}
