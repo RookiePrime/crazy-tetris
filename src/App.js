b08921ba915b7d270a8d22d07c688ce9521fa04b
lisahuele <lisahuele@gmail.com>
2021-09-16 14:56:32 -0400

    proj set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Tetris Redux</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
