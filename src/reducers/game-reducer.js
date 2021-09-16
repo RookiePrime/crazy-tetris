b11cd369cd151c6e848c653bd0fea24528bb6eba
lisahuele <lisahuele@gmail.com>
2021-09-16 18:33:14 -0400

    implemented move left and right actions

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -9,21 +9,32 @@ import {
     GAME_OVER
 } from '../actions'
 
-import { defaultState } from '../utils'
+import { defaultState, nextRotation, canMoveTo } from '../utils'
 
 //define a function to handle the actions
 const gameReducer = (state = defaultState(), action) => {
+    const { shape, grid, x, y, rotation, nextShape, score, isRunning } = state;
+
     switch(action.type) {
         case ROTATE:
-    
+            const newRotation = nextRotation(shape, rotation)
+            if(canMoveTo(shape, grid, x, y, newRotation)) {
+                return { ...state, rotation: newRotation };
+            }
             return state
   
         case MOVE_RIGHT:
-  
+            //add 1 from x and check if the new position is possible
+            if(canMoveTo(shape, grid, x+1, y, rotation)) {
+                return { ...state, x: x+1 };
+            }
             return state
   
         case MOVE_LEFT:
-  
+            //subtract 1 from x and check if the new position is possible
+            if(canMoveTo(shape, grid, x-1, y, rotation)) {
+                return { ...state, x: x-1 };
+            }
             return state
   
         case MOVE_DOWN:
--- src/utils/index.js
+++ src/utils/index.js
@@ -16,138 +16,174 @@ export const gridDefault = () => {
 
 // Define block shapes and their rotations as arrays.
 export const shapes = [
-    // none
-    [[[0,0,0,0],
-      [0,0,0,0],
-      [0,0,0,0],
-      [0,0,0,0]]],
-  
-    // I
-    [[[0,0,0,0],
-      [1,1,1,1],
-      [0,0,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [0,1,0,0],
-      [0,1,0,0],
-      [0,1,0,0]]],
-  
-    // T
-    [[[0,0,0,0],
-      [1,1,1,0],
-      [0,1,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [1,1,0,0],
-      [0,1,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [1,1,1,0],
-      [0,0,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [0,1,1,0],
-      [0,1,0,0],
-      [0,0,0,0]]],
-  
-    // L
-    [[[0,0,0,0],
-      [1,1,1,0],
-      [1,0,0,0],
-      [0,0,0,0]],
-  
-     [[1,1,0,0],
-      [0,1,0,0],
-      [0,1,0,0],
-      [0,0,0,0]],
-  
-     [[0,0,1,0],
-      [1,1,1,0],
-      [0,0,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [0,1,0,0],
-      [0,1,1,0],
-      [0,0,0,0]]],
-  
-    // J
-    [[[1,0,0,0],
-      [1,1,1,0],
-      [0,0,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,1,0],
-      [0,1,0,0],
-      [0,1,0,0],
-      [0,0,0,0]],
-  
-     [[0,0,0,0],
-      [1,1,1,0],
-      [0,0,1,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [0,1,0,0],
-      [1,1,0,0],
-      [0,0,0,0]]],
-  
-    // Z
-    [[[0,0,0,0],
-      [1,1,0,0],
-      [0,1,1,0],
-      [0,0,0,0]],
-  
-     [[0,0,1,0],
-      [0,1,1,0],
-      [0,1,0,0],
-      [0,0,0,0]]],
-  
-    // S
-    [[[0,0,0,0],
-      [0,1,1,0],
-      [1,1,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [0,1,1,0],
-      [0,0,1,0],
-      [0,0,0,0]]],
-  
-    // O
-    [[[0,1,1,0],
-      [0,1,1,0],
-      [0,0,0,0],
-      [0,0,0,0]]]
+  // none
+  [[[0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]]],
+
+  // I
+  [[[0,0,0,0],
+    [1,1,1,1],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+    [[0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0]]],
+
+  // T
+  [[[0,0,0,0],
+    [1,1,1,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+    [[0,1,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+    [[0,1,0,0],
+    [1,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+    [[0,1,0,0],
+    [0,1,1,0],
+    [0,1,0,0],
+    [0,0,0,0]]],
+
+  // L
+  [[[0,0,0,0],
+    [1,1,1,0],
+    [1,0,0,0],
+    [0,0,0,0]],
+
+    [[1,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+    [[0,0,1,0],
+    [1,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+    [[0,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,0,0]]],
+
+  // J
+  [[[1,0,0,0],
+    [1,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+    [[0,1,1,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+    [[0,0,0,0],
+    [1,1,1,0],
+    [0,0,1,0],
+    [0,0,0,0]],
+
+    [[0,1,0,0],
+    [0,1,0,0],
+    [1,1,0,0],
+    [0,0,0,0]]],
+
+  // Z
+  [[[0,0,0,0],
+    [1,1,0,0],
+    [0,1,1,0],
+    [0,0,0,0]],
+
+    [[0,0,1,0],
+    [0,1,1,0],
+    [0,1,0,0],
+    [0,0,0,0]]],
+
+  // S
+  [[[0,0,0,0],
+    [0,1,1,0],
+    [1,1,0,0],
+    [0,0,0,0]],
+
+    [[0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,0,0]]],
+
+  // O
+  [[[0,1,1,0],
+    [0,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]]]
 ]
 
 // Random Shape
 export const random = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 export const randomShape = () => {
-    return random(1, shapes.length - 1)
+  return random(1, shapes.length - 1)
 }
 
 // function to return the default object with properties to reflect default game state
 export const defaultState = () => {
-    return {
-        grid: gridDefault(),
-        shape: randomShape(),
-        rotation: 0,
-        // put the shape in the center of the grid, above the top
-        x: 5,
-        y: -4,
-        nextShape: randomShape(),
-        score: 0,
-        speed: 1000,
-        // Tell the game that it's currently running
-        isRunning: true,
-        gameOver: false
+  return {
+      grid: gridDefault(),
+      shape: randomShape(),
+      rotation: 0,
+      // put the shape in the center of the grid, above the top
+      x: 5,
+      y: -4,
+      nextShape: randomShape(),
+      score: 0,
+      speed: 1000,
+      // Tell the game that it's currently running
+      isRunning: true,
+      gameOver: false
+  }
+}
+
+//rotate function
+export const nextRotation = (shape, rotation) => {
+  return (rotation + 1) % shapes[shape].length
+}
+
+export const canMoveTo = (shape, grid, x, y, rotation) => {
+  const currentShape = shapes[shape][rotation];
+
+  //loop through all rows and cols of the shape
+  for (let row = 0; row < currentShape.length; row++) {
+    for(let col = 0; col < currentShape[row].length; row++) {
+      //look for a 1
+      if(currentShape[row][col] !== 0) {
+        const proposedX = col + x;
+        const proposedY = row + y;
+        if(proposedY < 0) {
+          continue;
+        }
+
+        //get the row on the grid
+        const possibleRow = grid[proposedY];
+        
+        //check if row exists
+        if(possibleRow) {
+          if(possibleRow[proposedX] === undefined || possibleRow[proposedX] !== 0) {
+            return false;
+          }
+        }
+      } else {
+        return false;
+      }
     }
+  }
+  return true;
 }
