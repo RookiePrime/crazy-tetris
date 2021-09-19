45cabf5932045ed91aa744d372ec1c35d5493cb6
lisahuele <lisahuele@gmail.com>
2021-09-19 18:18:25 -0400

    styling pause modal

--- src/components/PausePopup.js
+++ src/components/PausePopup.js
@@ -1,24 +1,45 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { resume, restart } from '../actions';
+import { faUser } from "@fortawesome/free-solid-svg-icons"; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function PausePopup(props) {
   const isRunning = useSelector((state) => state.game.isRunning);
   const gameOver = useSelector((state) => state.game.gameOver);
+  const dispatch = useDispatch();
 
   let message = '';
   let isHidden = 'hidden';
 
   if(gameOver) {
-    message = 'Game Over';
+    message = 'GAME OVER';
     isHidden = '';
   } else if (!isRunning) {
-    message = 'Pause';
+    message = 'GAME PAUSED';
     isHidden = '';
   }
 
   return (
     <div className={`pause-popup ${isHidden}`}>
-      <h1> { message } </h1>
+      <h1> {message} </h1>
+
+      <button className="popup-button" onClick={(e) => {
+        if(gameOver) { dispatch(restart()) }
+        if(!isRunning) { dispatch(resume()) }
+      }}>
+        { gameOver ? 'RESTART' : 'RESUME' }
+      </button>
+
+      <button className="popup-button" onClick={(e) => {
+        //return home
+      }}>QUIT GAME</button>
+
+      <p>
+        <FontAwesomeIcon icon={faUser} />
+        USER
+      </p>
+
     </div>
   )
 }
